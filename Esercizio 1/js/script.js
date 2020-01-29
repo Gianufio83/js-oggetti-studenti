af0827de56f027769b34ce3436e9eab4846e7787
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-29 14:46:15 +0100

    esercizio 1 punto uno

---
+++ Esercizio 1/js/script.js
@@ -0,0 +1,18 @@
+// alert('Ciao');
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+var studente = {
+  'nome' : 'Mario',
+  'cognome' : 'Neri',
+  'eta' : 25
+};
+for (var chiave in studente) {
+   console.log(studente[chiave]);
+}
+
+// Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+
+
+
+
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
