5a5b2f24f23df75a6194c5b522924a93283a3716
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-29 15:09:49 +0100

    esercizio 1 parte seconda

--- Esercizio 1/js/script.js
+++ Esercizio 1/js/script.js
@@ -10,9 +10,33 @@ for (var chiave in studente) {
 }
 
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-
-
-
+var studenti = [
+  {
+    'nome' : 'Marco',
+    'cognome' : 'Rossi',
+    'eta' : 21
+  },
+  {
+    'nome' : 'Gabriele',
+    'cognome' : 'Verdi',
+    'eta' : 23
+  },
+  {
+    'nome' : 'Mattia',
+    'cognome' : 'Neri',
+    'eta' : 22
+  },
+];
+console.log(studenti);
+for (var i = 0; i < studenti.length; i++) {
+  console.log(studenti[i]);
+  var primo = studenti[0];
+  console.log(primo.nome, primo.cognome);
+  var secondo = studenti[1];
+  console.log(secondo.nome, secondo.cognome);
+  var terzo = studenti[2];
+  console.log(terzo.nome, terzo.cognome);
+}
 
 
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
