0efffd3ca9abc30652fd504f7885f376ada2db7b
Gianufio83 <fiore.gianuario@gmail.com>
2020-01-29 16:34:12 +0100

    esercizio 2 -handlebars

---
+++ Esercizio 2/handlebars/js/script.js
@@ -0,0 +1,21 @@
+// alert('Ciao');
+// una pagina semplice con un input e un pulsante e inviate un messaggio a cui segue una risposta come avete fatto per boolzapp, usando però handlebars.
+$(document).ready(function () {
+  $('.invia').click(function () {
+    var risposta = {
+      'answer' : 'Ti chiamo più tardi',
+      'time'  : time
+    };
+    var data = new Date();
+      var hours = data.getHours();
+      var minutes = data.getMinutes();
+      var time = hours +':'+ minutes;
+
+    var source = $('#entry-template').html();
+    var template = Handlebars.compile(source);
+
+    var html = template(risposta);
+    $('#received-message').append(html);
+  });
+
+});
